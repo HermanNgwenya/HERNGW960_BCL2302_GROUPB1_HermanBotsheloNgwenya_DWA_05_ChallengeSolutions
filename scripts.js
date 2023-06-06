cee4f2f7055f0d2fd6cc49c979f422167e40f13d
HermanNgwenya <hermanbngwenya@gmail.com>
2023-06-06 15:37:09 +0200

    DWA_05

---
+++ scripts.js
@@ -0,0 +1,42 @@
+// scripts.js
+
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+
+  // removing the decimal values
+  const quotient = dividend / divider;
+  result.innerText = Math.floor(quotient);
+
+  // validation when the values are missing
+  if(!dividend || !divider){
+    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
+
+  if(dividend === "" || divider === "") {
+     result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+     return;
+  }
+
+  // for invalid divisions using 0 and negative numbers
+  if (divider <= 0) {
+    result.innerText = "Division not performed. Invalid number provided. Try again.";
+    console.error("Division by zero");
+    console.trace("Call stack");
+    return;
+  }
+
+  // validation for non-numbers
+  if(isNaN(dividend)|| isNaN(divider)){
+    console.error("Invalid nummber provided");
+    console.trace("Call stack");
+    document.body.innerText = "Something critical went wrong. Please reload the page.";
+  }
+  
+});
+
